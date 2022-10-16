7f3376c328495e3a6230dea251b9eeaf87b89317
hellocit <bestoosaka@gmail.com>
2022-10-16 22:46:43 +0900

    second

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # zsh_install
+
+zsh_install0.bash
+
+cd 
+
+vi .zshrc
+
+secret0.txtの中身を書き込み
+
+source ~/.zshrc
---
+++ zsh_install1.bash
@@ -0,0 +1,7 @@
+
+
+
+参考文献
+https://qiita.com/hiesiea/items/860c42a96b031f929b94
+
+https://qiita.com/d-dai/items/d7f329b7d82e2165dab3
